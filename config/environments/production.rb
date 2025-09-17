9f9cb35068865d436a6a35bab1474e8435978f2e
Robert Sęk <seku@users.noreply.github.com>
2025-09-17 20:55:58 +0200

    rec-parts hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << "rec-parts.com"
+  config.hosts << "www.rec-parts.com"
 end
